c94dc8deb0585308709a25882857b8851f0850f1
Anders Bjerner <abjerner@skybrud.dk>
2019-09-02 14:08:55 +0200

    Build stuff

---
+++ release.bat
@@ -0,0 +1,2 @@
+@echo off
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\msbuild.exe" src/Skybrud.UmbracoEssentials/Skybrud.UmbracoEssentials.csproj /t:rebuild /t:pack /p:Configuration=Release /p:PackageOutputPath=../../releases/nuget
